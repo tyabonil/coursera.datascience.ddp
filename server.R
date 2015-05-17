7bac3103134d63149d116c50cf15736db280f4bd
TyAbonil <ty@abonil.com>
2015-05-16 21:05:02 -0400

    working draft

--- server.R
+++ server.R
@@ -12,20 +12,17 @@ mush.train <- rbind(mush.train, mush.test)
 drops <- c("veil.type")
 mush.train <- mush.train[, !(names(mush.train) %in%drops)]
 
-mush.frm <- edible ~ odor + spore.print.color + population
-mush.fit <- nnet(as.formula(mush.frm), data=mush.train, method = "nnet", maxit = 100,  trace = F, size=4) 
+mush.frm <- edible ~ odor + spore.print.color
+mush.fit <- nnet(as.formula(mush.frm), data=mush.train, method = "nnet", maxit = 100,  trace = F, size=1, decay=0.1) 
 
 shinyServer(function(input, output) {
 
-  df <- data.frame(odor=NA, spore.print.color=NA, population=NA)
-  #df$odor <- reactive(input$odor)
-  #df$spore.print.color <- reactive(input$spore.print.color)
-  #df$population <- reactive(input$population)
-  #print(df)
+  df <- data.frame(odor=NA, spore.print.color=NA)
+
   output$fit <- renderText({
     paste("The mushroom is ", 
           switch(predict(mush.fit,
-                         newdata=data.frame(odor=input$odor, spore.print.color=input$spore.print.color, population=input$population),
+                         newdata=data.frame(odor=input$odor, spore.print.color=input$spore.print.color),
                          type="class"), 
           'e' = 'edible!',
           'p' = 'poisinous!',
--- ui.R
+++ ui.R
@@ -19,18 +19,12 @@ shinyUI(fluidPage(
                selectInput("spore.print.color", label = h3("Spore Print Color"), 
                            choices = list("black"="k","brown"="n","chocolate"="h","green"="r", "purple"="u","white"="w" ),
                            selected = 1))
-      ),
-      fluidRow(
-        column(12,
-               selectInput("population", label = h3("population"), 
-                           choices = list("abundant"="a","clustered"="c","numerous"="n", "scattered"="s","several"="v","solitary"="y" ),
-                           selected = 1))
       )
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      p('This app uses the nnet package to create a 4-layer artificial neural network trained using the UCI Machine Learning Repoository\'s Mushroom Data Set to predict whether a mushroom is poisinous or edible.'),
+      p('This app uses the nnet package to create an artificial neural network trained using the UCI Machine Learning Repoository\'s Mushroom Data Set to predict whether a mushroom is poisinous or edible.'),
       h3(textOutput("fit")),
       p(a("UCI Machine Learning Repository", "https://archive.ics.uci.edu/ml/datasets/Mushroom")),
       p(a("NNET Package", "hhttp://cran.r-project.org/web/packages/nnet/nnet.pdf"))
