9a2ed1b757eaa28d6aef7b559f78872ffe67410e
TyAbonil <ty@abonil.com>
2015-05-18 21:00:11 -0400

    created RDS file to aid in shiny app

--- server.R
+++ server.R
@@ -1,20 +1,6 @@
-library(nnet)
 library(shiny)
-
-# Define server logic required to draw a histogram
-## Read the data for each file
-mush.train  <- read.csv("mush.train.csv")
-mush.test <- read.csv("mush.test.csv")
-
-mush.train <- rbind(mush.train, mush.test)
-
-## Get rid of column with only one level 
-drops <- c("veil.type")
-mush.train <- mush.train[, !(names(mush.train) %in%drops)]
-
-mush.frm <- edible ~ odor + spore.print.color
-mush.fit <- nnet(as.formula(mush.frm), data=mush.train, method = "nnet", maxit = 100,  trace = F, size=1, decay=0.1) 
-
+## read the model
+mush.fit  <- readRDS("mush.fit.rds")
 shinyServer(function(input, output) {
 
   df <- data.frame(odor=NA, spore.print.color=NA)
